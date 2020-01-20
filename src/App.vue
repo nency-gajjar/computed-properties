9186ab4cb34cf6c6e08e65ece117ab0a6a300d97
Nency Gajjar <nency.bhadeshiya@bacancytechnology.com>
2020-01-20 14:08:32 +0530

    computed properties upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Computed Properties</h1>
+    <button @click="a++">Add to A</button>
+    <button @click="b++">Add to B</button>
+    <p>A = {{a}}</p>
+    <p>B = {{b}}</p>
+    <p>Age + A = {{addToAbyM()}}</p>
+    <p>Age + B = {{addToBbyM()}}</p>
+    <p>Age + A = {{addToAbyC}}</p>
+    <p>Age + B = {{addToBbyC}}</p>
+    <button @click="watchme">Watch Me</button>
+    <p>Age = {{age}}</p>
+    <hr>
+    <input v-model="name"/>
+    <p>{{"Method : " + lowerCase()}}</p>
+    <p>{{"Computed : " + lowerCaseName}}</p>
+    <p>{{"Watch : " + lcName}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      age:20,
+      a:0,
+      b:0,
+      name :'NENCY',
+      lcName : 'nency'
+    }
+  },
+
+  methods: {
+    addToAbyM(){
+      console.log('method:addToA')
+      return this.a + this.age
+    },
+    addToBbyM(){
+      console.log('method:addToB')
+      return this.b + this.age
+    },
+    watchme(){ 
+       this.age++;
+    },
+    lowerCase(){
+      return this.name.toLowerCase();
+    }
+  },
+
+  computed: {
+    addToAbyC(){
+      console.log('computed:addToA')
+      return this.a + this.age
+    },
+    addToBbyC(){
+      console.log('computed:addToB')
+      return this.b + this.age
+    },
+    lowerCaseName(){
+      return this.name.toLowerCase();
+    }
+
+  },
+
+  watch: {
+    age: function(newValue,oldValue){
+      console.log('watch:age changed')
+      console.log(newValue,oldValue)
+    },
+    name: function(){
+      this.lcName = this.name.toLowerCase();
+      console.log(this.lcName)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+button{
+  margin : 2px;
+}
+
+p{
+  margin-left: 2px;
 }
 </style>
